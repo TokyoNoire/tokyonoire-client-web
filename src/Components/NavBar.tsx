156e99dbf723349c68226fdf886eebdeaf8c2c26
Rei <reidumand@outlook.com>
2022-12-01 17:33:12 +0900

    Feat: add fontpacks & mofidy css config

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,11 +1,11 @@
 import React, {type FC, type ReactElement} from "react";
-import logo from '../../public/Logo_DarkTheme.svg'
+import logo from '../../public/Logo_DarkTheme.svg';
 
 const NavBar: FC = (): ReactElement => {
+
   return (
-    <div>
-      <img src={logo} alt="logo menu button"/>
-      <h1>NavBar</h1>
+    <div className="navbar">
+      <img src={logo.src} alt="logo menu button" className="logo"/>
     </div>
   );
 };
