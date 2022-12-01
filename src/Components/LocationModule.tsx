7299c7abf082f4adecde5dc18402b6a5340dae7a
Rei <reidumand@outlook.com>
2022-12-01 14:21:54 +0900

    Feat: Add GeoAPI. Works on Chrome Desktop

--- src/Components/LocationModule.tsx
+++ src/Components/LocationModule.tsx
@@ -1,9 +1,21 @@
-import React, {type FC, type ReactElement} from "react";
+import React, {type FC, type ReactElement, useEffect, useState, useRef} from "react";
+import { boolean } from "zod";
 
 const LocationModule: FC = (): ReactElement => {
 
+  let hasMounted = useRef<Boolean>(false);
 
+  useEffect(() => {
+    if (!hasMounted.current) {
+      if ('geolocation' in navigator) {
+        console.log('geolocation available')
+        navigator.geolocation.getCurrentPosition((position) => {console.log(position.coords.latitude, position.coords.longitude)});
+      }
+      else console.log('geolocation unavailable')
+    }
 
+    hasMounted.current = true;
+  }, [hasMounted])
 
   return (
     <div>
