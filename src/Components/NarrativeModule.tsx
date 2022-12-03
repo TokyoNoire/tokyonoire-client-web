765fc8a8ed55601f7aa242dc05d73368ac4dd263
kristinetuazon <kristine.jane.tuazon@gmail.com>
2022-12-03 15:23:04 +0900

    chore: pull from dev

--- src/Components/NarrativeModule.tsx
+++ src/Components/NarrativeModule.tsx
@@ -6,6 +6,26 @@ import heroImage from "public/Hero.jpg";
 const NarrativeModule: FC = (): ReactElement => {
 
 
+
+
+
+//   <Card>
+//   <div className="self-center">
+//     <h1 className="self-center p-5 text-2xl text-center uppercase font-heading">
+//       The salad murder mysteries
+//     </h1>
+//     <p className = "self-center font-heading">Salad is Life</p>
+//     <div className="self-center w-10/12 m-4">
+//       <Image src={heroImage} alt="Tokyo Noire Hero" className="rounded-lg" />
+//     </div>
+//     <p className="mt-2 text-left px-14 font-body1">Someone&apos;s come up missing, and there is a murder in the air. This scandalous discovery is only the tip of the iceberg. Salad is usually life, but it can also mean death.</p>
+//   </div>
+//   <CardActions>
+//   <button id="themeButton" className="mt-20 mb-5 font-body2" type="button">
+//   start
+// </button>
+//   </CardActions>
+// </Card>
   
   return (
     <div className="self-center w-4/5 m-10">
--- src/pages/game/index.tsx
+++ src/pages/game/index.tsx
@@ -1,4 +1,4 @@
-import React, {type FC, type ReactElement, useState} from "react";
+import React, { type FC, type ReactElement, useState } from "react";
 import {
   Dialog,
   DialogTitle,
@@ -19,24 +19,45 @@ const Game: FC = (): ReactElement => {
   };
   return (
     <div>
-            <div>
+      <div>
         <Dialog open={open} onClose={handleClose}>
           <DialogTitle>
-            <h1 className="self-center p-5 text-2xl text-center uppercase font-heading">Instructions</h1>
+            <h1 className="self-center p-5 text-3xl text-center uppercase font-heading">
+              How to Play
+            </h1>
           </DialogTitle>
           <DialogContent>
             <DialogContentText>
-              <p className="text-center font-body1"> hello test hello</p>
+              <p className="text-left font-body1">
+                This is a welcome message for you the kind player.
+                <br/>
+                <br/>
+                Here we will
+                need to think of our instructions. It needs to in simple words
+                explain the “go somewhere, resolve a challenge” principle we are
+                going with. 
+                <br/>
+                <br/>
+                Also explain that some interactions will require
+                user to grant authorisation for geolocation, webcam and/or
+                gyrometer."
+            
+              </p>
             </DialogContentText>
           </DialogContent>
           <DialogActions>
-          <button onClick={handleClose} id="themeButton" className="my-5 font-body2" type="button">
-        start
-      </button>
+            <button
+              onClick={handleClose}
+              id="themeButton"
+              className="my-5 font-body2"
+              type="button"
+            >
+              I understand
+            </button>
           </DialogActions>
         </Dialog>
       </div>
-      <LocationModule/>
+      <LocationModule />
     </div>
   );
 };
