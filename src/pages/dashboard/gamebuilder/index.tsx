fe72ce324f58b76da93fdd1c2a2a46fd98e2f9a8
kristinetuazon <kristine.jane.tuazon@gmail.com>
2022-12-04 22:28:04 +0900

    feat: dashboard/gamebuilder pages

--- src/pages/dashboard/gamebuilder/index.tsx
+++ src/pages/dashboard/gamebuilder/index.tsx
@@ -1,6 +1,6 @@
 import { type NextPage } from "next";
-import SignInForm from "../../Components/SignInForm";
-import SignUpForm from "../../Components/SignUpForm";
+import SignInForm from "../../../Components/SignInForm";
+import SignUpForm from "../../../Components/SignUpForm";
 // import Link from "next/link";
 
 const GameBuilder: NextPage = () => {
