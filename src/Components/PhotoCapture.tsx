ef686d6dffa4f7ac1794507960d78592269ed4a8
kristinetuazon <kristine.jane.tuazon@gmail.com>
2022-12-05 17:49:39 +0900

    fix: error

--- src/Components/PhotoCapture.tsx
+++ src/Components/PhotoCapture.tsx
@@ -1,14 +1,14 @@
-import React, { type FC, type ReactElement, useState, useRef } from "react";
-import { Camera } from "react-camera-pro";
+// import React, { type FC, type ReactElement, useState, useRef } from "react";
+// import { Camera } from "react-camera-pro";
 
-const PhotoCapture: FC = (): ReactElement => {
-  //   const camera = useRef(null);
-  //   const [image, setImage] = useState(null);
-   return <div>
-  {/* //       <Camera ref={camera} />
-  //     <button onClick={() => setImage(camera.current.takePhoto())}>Take photo</button>
-  //     <img src={image} alt='Taken photo'/> */}
-  </div>;
-};
+// const PhotoCapture: FC = (): ReactElement => {
+//     const camera = useRef(null);
+//     const [image, setImage] = useState(null);
+//    return <div>
+//          <Camera ref={camera} />
+//        <button onClick={() => setImage(camera.current.takePhoto())}>Take photo</button>
+//        <img src={image} alt='Taken photo'/>
+//   </div>;
+// };
 
-export default PhotoCapture;
+// export default PhotoCapture;
