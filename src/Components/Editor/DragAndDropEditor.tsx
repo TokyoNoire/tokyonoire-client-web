a22161cab892b2f780b10fa2e5376a4b3503c18a
kristinetuazon <kristine.jane.tuazon@gmail.com>
2022-12-08 15:14:42 +0900

    chore: intial commit

---
+++ src/Components/Editor/DragAndDropEditor.tsx
@@ -0,0 +1,12 @@
+import React, {type FC, type ReactElement} from "react";
+
+
+const DragAndDropEditor: FC = (): ReactElement => {
+  return (
+    <div className="w-full h-full px-6 py-4 ">
+     <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default DragAndDropEditor;
