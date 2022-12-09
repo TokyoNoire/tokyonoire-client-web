dd55afb981a3400d54babe0cac7dd358077ec2af
kristinetuazon <kristine.jane.tuazon@gmail.com>
2022-12-09 19:07:24 +0900

    feat: added dnd sort with module objects, added useLocalStorage and useSessionStorage hooks

--- src/Components/Editor/DragAndDropEditor.tsx
+++ src/Components/Editor/DragAndDropEditor.tsx
@@ -1,13 +1,14 @@
 import React, {type FC, type ReactElement, useState, useCallback} from "react";
-
 import { DndContext } from "@dnd-kit/core";
 import { arrayMove, SortableContext, useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
 
-const gameModules = [{id:1, string:"hello"}, {id:2, string:"friend"}, {id:3, string:"turtle"}, {id:4, string:"poop"}];
-const contents = gameModules.map((gameModule) => ({
-  id: gameModule.id,
-  content: gameModule.string,
+
+const gameModules = [ {_id:231, typeOfModule: "location", title: "Akika Spotted", question: "", answer:"", image:"", description: "The last time she was seen was sitting on an unusual bench near the Mori Tower, in Roppongi.", locationCoordinates: [35.659439, 139.728384]}, {_id: 312, typeOfModule: "location", title: "Akika Spotted", question: "", answer:"", image:"", description: "The last time she was seen was sitting on an unusual bench near the Mori Tower, in Roppongi.", locationCoordinates: [35.659439, 139.728384]}];
+const contents = gameModules.map((gameModule,index) => ({
+  id: index,
+  moduleId: gameModule._id,
+  title: gameModule.title,
 }));
 
 interface SortableItemProps {
@@ -25,17 +26,17 @@ function SortableItem({ id, children }: SortableItemProps) {
 
   return (
     // eslint-disable-next-line react/jsx-props-no-spreading
-    <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
+    <div ref={setNodeRef} className="h-52 bg-stone-400 w-52" style={style} {...attributes} {...listeners}>
       {children}
     </div>
   );
 }
 
 const DragAndDropEditor: FC = (): ReactElement => {
-  const [state, setState] = useState<{ id: number; content: string }[]>(contents);
-console.log(state)
+  const [state, setState] = useState<{id:number, title:string, moduleId: number}[]>(contents);
+
   const handleDragEnd = useCallback(
-    (event:any) => {
+    (event: any) => {
       const { active, over } = event;
       if (over === null) {
         return;
@@ -63,9 +64,9 @@ console.log(state)
       <SortableContext items={state}>
         <div className="w-full h-full flexCenterDiv">
           {state.map((item) => (
-            <SortableItem key={item.id} id={item.id}>
+            <SortableItem key={item.moduleId} id={item.id}>
               <div>
-                {item.content}
+                {item.title}
               </div>
             </SortableItem>
           ))}
