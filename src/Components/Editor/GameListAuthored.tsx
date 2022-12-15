1b80ccbbd86b0d592b61b2a41c29278fe7518112
kristinetuazon <kristine.jane.tuazon@gmail.com>
2022-12-15 19:50:22 +0900

    style: list style change

--- src/Components/Editor/GameListAuthored.tsx
+++ src/Components/Editor/GameListAuthored.tsx
@@ -1,113 +1,116 @@
-import React, { type FC, type ReactElement, useState, useEffect, useRef } from "react";
-import {
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogContentText,
-  DialogActions,
-  Box
-} from "@mui/material";
+import React, {
+  type FC,
+  type ReactElement,
+  useState,
+  useEffect,
+  useRef,
+} from "react";
+import { Box } from "@mui/material";
 import { saveGameInfo } from "../../types/global";
-import { DataGrid, GridColDef, GridValueGetterParams, GridValueFormatterParams } from '@mui/x-data-grid';
+import DeleteIcon from "@mui/icons-material/Delete";
+import ModeEditIcon from "@mui/icons-material/ModeEdit";
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import {
+  DataGrid,
+  GridColDef,
+  GridValueFormatterParams,
+  GridRowParams,
+  GridActionsCellItem,
+} from "@mui/x-data-grid";
 import axios from "axios";
 import moment from "moment";
+import PublishIcon from '@mui/icons-material/Publish';
 
-const AuthoredListPopup: FC = (): ReactElement => {
-  const [open, setOpen] = useState<boolean>(true);
-  const [gamesAuthored, setGamesAuthored] = useState<saveGameInfo[] | null>(null)
-  const hasMounted = useRef<boolean>(false);
-  
-  const handleClose = () => {
-    setOpen(false);
-  };
-  
-  const getPublicGame = async () => {
-    await axios
-      .get("https://tokyo-noire-server-development.herokuapp.com/test")
-      .then((response) => {
-        setGamesAuthored(response.data);
-      });
-  };
+const GameListAuthored: FC = (): ReactElement => {
 
-  useEffect(() => {
-    if (!hasMounted.current && !gamesAuthored) {
-      getPublicGame();
-      hasMounted.current = true;
-    }
-  }, [hasMounted]);
+  const columns: GridColDef[] = [
+    {
+      field: "titleOfGame",
+      headerName: "Case Name",
+      minWidth: 300,
+      flex: 1,
+    },
 
-console.log(gamesAuthored);
+    {
+      field: "isPrivate",
+      headerName: "Visibility",
+      type: "boolean",
+      description: "This column has a value getter and is not sortable.",
+      sortable: true,
+      minWidth: 100,
+      flex: 2,
+    },
+    {
+      field: "isPublished",
+      headerName: "Published",
+      type: "boolean",
+      sortable: true,
+      minWidth: 100,
+      flex: 3,
+    },
+    {
+      field: "dateCreated",
+      headerName: "Created On",
+      type: "date",
+      minWidth: 150,
+      sortable: true,
+      flex: 4,
+      valueFormatter: (params: GridValueFormatterParams) =>
+        `${moment(params?.value).format("DD/MM/YYYY hh:mm A")}`,
+    },
+    {
+      field: "dateUpdated",
+      headerName: "Last Modified",
+      type: "date",
+      minWidth: 150,
+      sortable: true,
+      flex: 5,
+      valueFormatter: (params: GridValueFormatterParams) =>
+        `${moment(params?.value).format("DD/MM/YYYY hh:mm A")}`,
+    },
+    {
+      minWidth: 200,
+      field: "actions",
+      type: "actions",
+      flex: 6,
+      /* @ts-ignore */
+      getActions: (params) => [
+        <GridActionsCellItem key="1" icon={<ModeEditIcon />} label="Edit" />,
+        <GridActionsCellItem key="2" icon={<PublishIcon />} label="Publish" />,
+        <GridActionsCellItem key="3" icon={<VisibilityIcon />} label="Visibility" />,
+        <GridActionsCellItem key="4" icon={<DeleteIcon />} label="Delete" />,
+      ],
+    },
+  ];
 
-const columns: GridColDef[] = [
-  { field: 'id', headerName: 'ID', width: 90 },
-  {
-    field: 'titleOfGame',
-    headerName: 'Case Name',
-    width: 150,
-    editable: true,
-  },
-  {
-    field: 'dateCreated',
-    headerName: 'Created On',
-    type: 'date',
-    width: 150,
-    valueFormatter: (params: GridValueGetterParams) => 
-     moment(params?.value).format("DD/MM/YYYY hh:mm A"),
-
-  },
-  {
-    field: 'dateUpdated',
-    headerName: 'Last Modified',
-    type: 'date',
-    width: 150,
-    valueFormatter: (params: GridValueGetterParams) => 
-    moment(params?.value).format("DD/MM/YYYY hh:mm A")
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params: GridValueGetterParams) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-  },
-];
-
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
-
+  const rows = [
+    { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
+    { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+    { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+    { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+    { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+    { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+    { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+    { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+    { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+  ];
 
   return (
-    <div className="w-full h-full mt-5 overflow-x-auto flexCenterDiv">
-        <p className="self-center p-5 text-xl text-center uppercase font-heading">Welcome USERNAME</p>
-        <DialogContent>
-          <p className="text-center font-body1">
-          &quot;Is there a mystery afoot that you&apos;re itching for others to solve?&quot;
-          </p>
-          </DialogContent>
-        <Box sx={{ height: 400, width: '100%' }}>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
-        checkboxSelection
-        disableSelectionOnClick
-        experimentalFeatures={{ newEditingApi: true }}
-      />
-    </Box>
-    </div>
+    <>
+
+      <Box sx={{ height: 400}}
+  justifyContent="center"
+  alignItems="center">
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          pageSize={5}
+          rowsPerPageOptions={[5]}
+          disableSelectionOnClick
+        />
+      </Box>
+      </>
   );
 };
 
-export default AuthoredListPopup;
+export default GameListAuthored;
--- src/pages/editor/index.tsx
+++ src/pages/editor/index.tsx
@@ -3,7 +3,7 @@ import { useRouter } from "next/router";
 import { type saveGameInfo } from "../../types/global";
 import { Button, Menu, MenuItem } from "@mui/material";
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
-import GameListAuthored from "src/Components/Editor/GameListAuthored";
+import GameListAuthored from "../../Components/Editor/GameListAuthored"
 
 interface Props {
     game: saveGameInfo;
@@ -35,84 +35,22 @@ const Editor = (props: Props): ReactElement => {
 
     return (
         <>
-            <div className="mt-40 overflow-x-auto">
+            <div className="mt-40 overflow-x-auto flexCenterDiv">
+            <p className="self-center p-5 text-xl text-center uppercase font-heading">
+        Welcome USERNAME
+      </p>
+      <p className="mb-10 text-center font-body1">
+        &quot;Is there a mystery afoot that you&apos;re itching for others to
+        solve?&quot;
+      </p>
+            <div className="flex w-full p-5 justify-right font-heading ">
+                <button id="themeButton" onClick={handleCreateNewGameClick}>Open New Case</button>
+            </div>
         <GameListAuthored/>
             </div >
 
-            <div className="flex justify-center p-10">
-                <button id="themeButton" onClick={handleCreateNewGameClick}>Open New Case</button>
-            </div>
         </>
     )
 };
 
 export default Editor;
-
-{/* <table className="w-full text-sm text-center ">
-    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-        <tr>
-            <th scope="col" className="px-2 py-3 font-body2">
-                case title
-            </th>
-            <th scope="col" className="px-2 py-3 font-body2">
-                id
-            </th>
-            <th scope="col" className="px-2 py-3 font-body2">
-                created on
-            </th>
-            <th scope="col" className="px-2 py-3 font-body2">
-                last modified
-            </th>
-            <th scope="col" className="px-2 py-3 font-body2">
-
-            </th>
-
-        </tr>
-    </thead>
-    <tbody>
-        <tr className="bg-white border-b">
-            <th
-                scope="row"
-                className="px-2 py-2 font-heading whitespace-nowrap"
-            >
-                Placeholder Case Name
-            </th>
-            <td className="px-2 py-4 font-heading">Placeholder ID</td>
-            <td className="px-2 py-4 font-heading">Placeholder creation date</td>
-            <td className="px-2 py-4 font-heading">
-                Placeholder creation date
-            </td>
-            <td className="px-2 py-4 font-heading">
-                <button id="themeButton">Edit</button>
-                <Button
-                    sx={{
-                        borderRadius: "9999px",
-                        height: "2rem",
-                        width: "2rem",
-                    }}
-                    id="basic-button"
-                    aria-controls={open ? 'basic-menu' : undefined}
-                    aria-haspopup="true"
-                    aria-expanded={open ? 'true' : undefined}
-                    onClick={handleClick}
-                >
-                    <MoreHorizIcon />
-                </Button>
-                <Menu
-                    id="basic-menu"
-                    anchorEl={anchorEl}
-                    open={open}
-                    onClose={handleClose}
-                    MenuListProps={{
-                        'aria-labelledby': 'basic-button',
-                    }}
-                >
-                    <MenuItem onClick={handleClose}>Unpublish</MenuItem>
-                    <MenuItem onClick={handleClose}>Set Private</MenuItem>
-                    <MenuItem onClick={handleClose} sx={{ color: "red" }}>Delete</MenuItem>
-                </Menu>
-
-            </td>
-        </tr>
-    </tbody>
-</table> */}
