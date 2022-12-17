7e7de48d53c8825b7b0d80b0577f61eaa725bcb7
kristinetuazon <kristine.jane.tuazon@gmail.com>
2022-12-18 00:23:23 +0900

    styling: shadow over grey

--- src/Components/GameModules/QuestionModule.tsx
+++ src/Components/GameModules/QuestionModule.tsx
@@ -1,6 +1,7 @@
 import React, { type ReactElement, useState } from "react";
 import { type GameModule } from "../../types/global";
 import TextField from "@mui/material/TextField";
+import HintPopper from "./Helpers/HintPopper";
 
 interface props {
   gameObject: GameModule;
@@ -13,32 +14,37 @@ const QuestionModule = (props: props): ReactElement => {
 
   return (
     <div>
-      <div className="self-center w-4/5 m-10 flexCenterDiv bg-darkGrey">
-
+      <div className="self-center w-full py-10 flexCenterDiv shadow-inset1">
         <div className="self-center mb-5 flexCenterDiv">
           <h1 className="self-center p-5 text-2xl text-center uppercase font-heading">
             {gameObject.title}
           </h1>
-
+          {/* 
           {gameObject.imageURL === null &&
-            // <Image
-            //   src={gameObject.image}
-            //   alt="Image for question module"
-            //   className="rounded-lg"
-            // ></Image>
             <img
               src={gameObject.imageURL}
               alt="Image for question module"
               className="rounded-lg"
             />
-          }
+          } */}
 
-          <p className="px-6 mt-2 font-body1">{gameObject.description}</p>
+          {gameObject.imageURL ? (
+            <img
+              src={gameObject.imageURL}
+              alt="Image for question module"
+              className="rounded-lg"
+            />
+          ) : (
+            ""
+          )}
+
+          <p className="px-6 mt-2 text-center font-body1">{gameObject.description}</p>
         </div>
-
       </div>
       <div className="items-center mx-20 my-48 flexCenterDiv">
-        <h1 className="mb-5 text-m font-heading">&quot;{gameObject.question}&quot;</h1>
+        <h1 className="mb-5 text-m font-heading">
+          &quot;{gameObject.question}&quot;
+        </h1>
         <TextField
           id="standard-basic"
           fullWidth
@@ -50,7 +56,7 @@ const QuestionModule = (props: props): ReactElement => {
 
         <button
           id="themeButton"
-          className="mt-5 font-heading"
+          className="self-center w-2/3 mt-20 mb-10 font-heading"
           type="button"
           onClick={() => {
             if (answer.toLowerCase() === gameObject.answer) {
@@ -61,6 +67,7 @@ const QuestionModule = (props: props): ReactElement => {
           is my answer
         </button>
       </div>
+      <HintPopper hint={gameObject.hint!}/>
     </div>
   );
 };
